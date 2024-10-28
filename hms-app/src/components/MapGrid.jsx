b94b0b707ef661b86416bad56f7bb6dbf349ad46
Martin Crabtree <martin.crabtree@gmail.com>
2024-10-28 14:05:56 -0400

    setting up pathing logic

--- hms-app/src/components/MapGrid.jsx
+++ hms-app/src/components/MapGrid.jsx
@@ -25,75 +25,92 @@ const _createRoomData = (data) => {
 
   return roomData;
 };
-function createMap() {
-  // Initialize a 6x6 array
-  const grid = Array.from({ length: 6 }, () =>
-    Array(6).fill({
-      id: "default-room-id",
-      type: "WALL",
-      isOpen: false,
-    })
-  );
-
-  // Start the path at [0][0] with START
-  grid[0][0] = {
-    ...grid[0][0],
-    type: "START",
-    isOpen: true,
-    id: `room-${getRandomInt(3000).toString()}`,
-    row: 0,
-    col: 0,
-  };
-
-  let row = 0;
-  let col = 0;
-
-  // Create a path to [5][5]
-  while (row !== 5 || col !== 5) {
-    const possibleMoves = [];
-
-    if (col < 5) {
-      possibleMoves.push("right");
-    }
-    if (row < 5) {
-      possibleMoves.push("down");
-    }
-
-    // Randomly choose a move
-    const move =
-      possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
-
-    if (move === "right") {
-      col++;
-    } else if (move === "down") {
-      row++;
-    }
-    grid[row][col] = {
-      ...grid[row][col],
-      type: "ROOM",
-      id: `room-${getRandomInt(3000).toString()}`,
-      row: row,
-      col: col,
-    };
-  }
-  grid[5][5] = {
-    ...grid[5][5],
-    type: "ROOM",
-    id: `room-${getRandomInt(3000).toString()}`,
-    row: 5,
-    col: 5,
-  };
-
-  return grid;
-}
 
 const MapGrid = () => {
+  // consider migrating to useReducer
   const [mapData, setMapData] = useState();
+  const [currentRoom, setCurrentRoom] = useState();
+  const [pathIDs, setPathIDs] = useState([]);
+  const [activeEncounter, setActiveEncounter] = useState(false);
+  // const [pathData, setPathData] = useState();
   useEffect(() => {
-    const newMap = createMap;
-    setMapData(newMap);
+    let pathArr = [];
+    function createMap() {
+      console.log("createMap", mapData);
+      // Initialize a 6x6 array
+      const grid = Array.from({ length: 6 }, () =>
+        Array(6).fill({
+          id: "default-room-id",
+          type: "WALL",
+          isOpen: false,
+        })
+      );
+
+      // Start the path at [0][0] with START
+      grid[0][0] = {
+        ...grid[0][0],
+        type: "START",
+        isOpen: true,
+        id: `room-${getRandomInt(3000).toString()}`,
+        row: 0,
+        col: 0,
+        nextRoom: undefined,
+      };
+      pathArr.push(grid[0][0].id);
+
+      let row = 0;
+      let col = 0;
+
+      // Create a path to [5][5]
+      while (row !== 5 || col !== 5) {
+        const possibleMoves = [];
+        const roomID = `room-${getRandomInt(3000).toString()}`;
+        grid[row][col].nextRoom = roomID;
+
+        if (col < 5) {
+          possibleMoves.push("right");
+        }
+        if (row < 5) {
+          possibleMoves.push("down");
+        }
+
+        // Randomly choose a move
+        const move =
+          possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+
+        if (move === "right") {
+          col++;
+        } else if (move === "down") {
+          row++;
+        }
+        grid[row][col] = {
+          ...grid[row][col],
+          type: "ROOM",
+          id: roomID,
+          row: row,
+          col: col,
+          nextRoom: undefined,
+        };
+        pathArr.push(roomID);
+      }
+      grid[5][5] = {
+        ...grid[5][5],
+        type: "BOSS",
+        id: `room-${getRandomInt(3000).toString()}`,
+        row: 5,
+        col: 5,
+      };
+      if (!grid[5][5].nextRoom) grid[5][5].nextRoom = grid[row][col].roomID;
+
+      pathArr.push(grid[5][5].id);
+      return grid;
+    }
+    !mapData && setMapData(createMap());
+    // !pathData && !!pathArr[0] && setPathData(pathArr);
   }, []);
   console.log("mapData", mapData);
+  console.log("currentRoom", currentRoom);
+  mapData && !currentRoom && setCurrentRoom(mapData[0][0]);
 
   const _handleOnClick = (row, col) => {
     console.log("row", row);
@@ -117,7 +134,7 @@ const MapGrid = () => {
           textAlign: "center",
           backgroundColor: `${isOpen ? "white" : "grey"}`,
         }}
-        onClick={(row, col) => _handleOnClick}
+        onClick={() => _handleOnClick(row, col)}
       >
         {type}
       </td>
